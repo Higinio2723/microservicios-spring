858fcc9b6e0e52b28c092d2a364f22cbb31bd906
Higinio2723 <52228319+Higinio2723@users.noreply.github.com>
2023-03-23 06:27:58 -0600

    Cambios realizado para la funcionalidad de los productos services

--- producto-service/src/main/java/com/msvc/producto/ProductoServiceApplication.java
+++ producto-service/src/main/java/com/msvc/producto/ProductoServiceApplication.java
@@ -1,9 +1,11 @@
-package com.producto;
+package com.msvc.producto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+//@EnableEurekaClient
 public class ProductoServiceApplication {
 
 	public static void main(String[] args) {
